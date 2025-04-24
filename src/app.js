0e7ef99321099635e30522642a76255f2a36c639
Otávio Katibe <otaviokatibe@gmail.com>
2025-04-24 02:17:34 +0000

    Tarefa 2.5 crud_mongoDB

--- src/app.js
+++ src/app.js
@@ -7,22 +7,17 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Conexão com o banco de dados
 connectDB();
 
-// Rotas
 app.use('/users', userRoutes);
 
-// Rota de teste
 app.get('/', (req, res) => {
   res.send('API de Usuários com MongoDB (CommonJS)');
 });
 
-// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
